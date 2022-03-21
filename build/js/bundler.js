04531daeff755c282a61823ded7dfdde4b5858ee
naeem252 <naeemhasan252@gmail.com>
2022-03-21 19:13:47 +0600

    fix some ui issue

--- build/js/bundler.js
+++ build/js/bundler.js
@@ -751,7 +751,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51323" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55071" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
